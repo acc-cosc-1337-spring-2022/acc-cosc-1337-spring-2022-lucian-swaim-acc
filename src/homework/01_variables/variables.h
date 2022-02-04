334932287fe9697a8a29c84f58e36a6983f882f7
lucian-swaim <lucian.swaim@g.austincc.edu>
2022-02-04 04:06:11 -0600

    hw-01-variables Submission #1

--- src/homework/01_variables/variables.h
+++ src/homework/01_variables/variables.h
@@ -2,7 +2,9 @@
 int add_numbers(int num1, int num2);
 
 
+/* a) Find file variables.h, create an int value-function prototype named multiply_numbers with one int parameter named num1. */
 
+int multiply_numbers(int num1);
 
 
 
