7e4f76d0e0306db77d06c5f381e7ef6c924005e5
lucian-swaim-acc <lucian.swaim@g.austincc.edu>
2022-02-11 22:23:28 -0600

    hw 2 w/ test cases

--- src/homework/02_expressions/expressions.cpp
+++ src/homework/02_expressions/expressions.cpp
@@ -12,3 +12,14 @@ int add_numbers(int num1, int num2)
 
 
 
+// write get_sales_tax_amount function code, multiply tax_rate and meal_amount and return their product.
+double get_sales_tax_amount (double meal_amount) {
+
+	return (tax_rate * meal_amount);
+}
+
+// write get_tip_amount function code to multiply  meal_amount and tip_rate, return their product.
+double get_tip_amount(double meal_amount, double tip_rate) {
+
+	return (meal_amount * tip_rate);
+}
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,15 +1,34 @@
 ﻿//write include statements
-
+#include "../src/homework/02_expressions/expressions.h"
+#include <iostream>
 
 //write namespace using statement for cout
-
+using std::cout, std::cin;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	double meal_amount, tip_rate, tip_amount, tax_amount, total;
+	cout << "Enter the amount for the meal:  ";
+	cin >> meal_amount;
+
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout << "Enter the tip rate in decimal format (example: 20% written as 0.20):  ";
+	cin >> tip_rate;
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	total = (tip_amount + tax_amount + meal_amount);
+
+
+	cout << "\n\n--Your Receipt--\n";
+	cout << "Meal Amount:      $" << meal_amount << "\n";
+	cout << "Sales Tax:        $" << tax_amount << "\n";
+	cout << "Tip Percentage:    " << (tip_rate*100) << "%\n";
+	cout << "Tip Amount:       $" << tip_amount << "\n";
+	cout << "Total:            $" << total << "\n";
 
 	return 0;
 }
