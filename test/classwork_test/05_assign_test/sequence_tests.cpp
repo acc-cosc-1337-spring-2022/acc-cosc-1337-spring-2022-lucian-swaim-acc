5fa15e99d76f415f8b587ab90bd98015f2a55c99
lucian-swaim-acc <lucian.swaim@g.austincc.edu>
2022-03-05 14:38:21 -0600

    Hw05 DNA Functions

--- test/classwork_test/05_assign_test/sequence_tests.cpp
+++ test/classwork_test/05_assign_test/sequence_tests.cpp
@@ -1,8 +1,35 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "loops.h"
+#include "sequence.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+
+TEST_CASE("Test function get_gc_content") {
+	REQUIRE(get_gc_content("AGCTATAG") == .375);
+	REQUIRE(get_gc_content("CGCTATAG") == .50);
+}
+
+TEST_CASE("Test function reverse_string") {
+	REQUIRE(reverse_string("AGCTATAG") == "GATATCGA");
+	REQUIRE(reverse_string("CGCTATAG") == "GATATCGC");
+}
+
+TEST_CASE("Test function get_dna_complement") {
+	REQUIRE(get_dna_complement("AAAACCCGGT") == "ACCGGGTTTT");
+	REQUIRE(get_dna_complement("CCCGGAAAAT") == "ATTTTCCGGG");
+}
+
+TEST_CASE("Test function validate_dna_string") {
+	REQUIRE(validate_dna_string("AAAACCCGGT") == true);
+	REQUIRE(validate_dna_string("CCCGGAAAAT") == true);
+	REQUIRE(validate_dna_string("AGCTATAG") == true);
+	REQUIRE(validate_dna_string("CGCTATAG") == true);
+	REQUIRE(validate_dna_string("badAGCTuserTGACinput") == false);
+	REQUIRE(validate_dna_string("aaaacccggt") == false);
+	REQUIRE(validate_dna_string("cccggaaaat") == false);
+	REQUIRE(validate_dna_string("agctatag") == false);
+	REQUIRE(validate_dna_string("cgctatag") == false);
+}
