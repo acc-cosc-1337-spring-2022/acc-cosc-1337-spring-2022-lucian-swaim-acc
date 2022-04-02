f6790a2bef64040091386615d097cacb6fa7a639
lucian-swaim-acc <lucian.swaim@g.austincc.edu>
2022-04-02 13:42:30 -0500

    Hw07 Complete

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -12,19 +12,45 @@ int main()
 	{
 		cout << "Enter first player (X or O): ";
 		cin >> first_player;
+
+		while ( (first_player != "X") && (first_player != "O") )
+		{
+			cout << "Invalid input. Please choose X or O.\n";
+			cin >> first_player;
+		}
+
 		game.start_game(first_player);
 
 		int position;
 		while(game.game_over() == false)
 		{
-			cout << "Enter position [1-9]";
+			cout << "Enter position [1-9]: ";
 			cin >> position;
+
+			while ( (position < 1) || (position > 9) )
+			{
+				cout << "Invalid input. Please pick a position between 1 and 9 (inclusive)\n";
+				cin >> position;
+			}
+
 
 			game.mark_board(position);
 			game.display_board();
+
+			if (game.game_over() == true)
+			{
+				if (game.get_winner() == "C")
+				{
+					cout << "Tie!\n";
+				}
+				else
+				{
+					cout << game.get_winner() << " wins!\n";
+				}
+			}
 		}
 
-		cout << "Continue? (Y/N): ";
+		cout << "Play another? (Y/N): ";
 		cin >> choice;
 
 	} while (choice == 'Y' || choice == 'y');
