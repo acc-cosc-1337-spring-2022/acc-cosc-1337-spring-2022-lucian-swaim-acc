106f3a177c3668199ff3b9015c4ffad7e6c727da
lucian-swaim-acc <lucian.swaim@g.austincc.edu>
2022-04-16 03:52:02 -0500

    Homework 9 w/ Test Cases v06

--- src/homework/tic_tac_toe/tic_tac_toe_4.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe_4.cpp
@@ -12,22 +12,42 @@ false
 */
 bool TicTacToe4::check_column_win()
 {
-    if      ( (pegs[0] == "X" || pegs[0] == "O") && (pegs[4] == "X" || pegs[4] == "O") && (pegs[8] == "X" || pegs[8] == "O") && (pegs[12] == "X" || pegs[12] == "O"))
+    // X column win
+    if      ( (pegs[0] == "X") && (pegs[4] == "X") && (pegs[8] == "X") && (pegs[12] == "X"))
     {
         return true;
     }
-    else if ( (pegs[1] == "X" || pegs[1] == "O") && (pegs[5] == "X" || pegs[5] == "O") && (pegs[9] == "X" || pegs[9] == "O") && (pegs[13] == "X" || pegs[13] == "O"))
+    else if ( (pegs[1] == "X") && (pegs[5] == "X") && (pegs[9] == "X") && (pegs[13] == "X"))
     {
         return true;
     }
-    else if ( (pegs[2] == "X" || pegs[2] == "O") && (pegs[6] == "X" || pegs[6] == "O") && (pegs[10] == "X" || pegs[10] == "O") && (pegs[14] == "X" || pegs[14] == "O"))
+    else if ( (pegs[2] == "X") && (pegs[6] == "X") && (pegs[10] == "X") && (pegs[14] == "X"))
     {
         return true;
     }
-    else if ( (pegs[3] == "X" || pegs[3] == "O") && (pegs[7] == "X" || pegs[7] == "O") && (pegs[11] == "X" || pegs[11] == "O") && (pegs[15] == "X" || pegs[15] == "O"))
+    else if ( (pegs[3] == "X") && (pegs[7] == "X") && (pegs[11] == "X") && (pegs[15] == "X"))
     {
         return true;
     }
+
+    // O Column win
+    else if ( (pegs[0] == "O") && (pegs[4] == "O") && (pegs[8] == "O") && (pegs[12] == "O"))
+    {
+        return true;
+    }
+    else if ( (pegs[1] == "O") && (pegs[5] == "O") && (pegs[9] == "O") && (pegs[13] == "O"))
+    {
+        return true;
+    }
+    else if ( (pegs[2] == "O") && (pegs[6] == "O") && (pegs[10] == "O") && (pegs[14] == "O"))
+    {
+        return true;
+    }
+    else if ( (pegs[3] == "O") && (pegs[7] == "O") && (pegs[11] == "O") && (pegs[15] == "O"))
+    {
+        return true;
+    }
+
     else
     {
         return false;
@@ -48,22 +68,42 @@ false
 */
 bool TicTacToe4::check_row_win()
 {
-    if      ((pegs[0] == "X" || pegs[0] == "O") && (pegs[1] == "X" || pegs[1] == "O") && (pegs[2] == "X" || pegs[2] == "O") && (pegs[3] == "X" || pegs[3] == "O"))
+    // X row win
+    if      ( (pegs[0] == "X") && (pegs[1] == "X") && (pegs[2] == "X") && (pegs[3] == "X"))
     {
         return true;
     }
-    else if ( (pegs[4] == "X" || pegs[4] == "O") && (pegs[5] == "X" || pegs[5] == "O") && (pegs[6] == "X" || pegs[6] == "O") && (pegs[7] == "X" || pegs[7] == "O"))
+    else if ( (pegs[4] == "X") && (pegs[5] == "X") && (pegs[6] == "X") && (pegs[7] == "X"))
     {
         return true;
     }
-    else if ( (pegs[8] == "X" || pegs[8] == "O") && (pegs[9] == "X" || pegs[9] == "O") && (pegs[10] == "X" || pegs[10] == "O") && (pegs[11] == "X" || pegs[11] == "O"))
+    else if ( (pegs[8] == "X") && (pegs[9] == "X") && (pegs[10] == "X") && (pegs[11] == "X"))
     {
         return true;
     }
-    else if ( (pegs[12] == "X" || pegs[12] == "O") && (pegs[13] == "X" || pegs[13] == "O") && (pegs[14] == "X" || pegs[14] == "O") && (pegs[15] == "X" || pegs[15] == "O") )
+    else if ( (pegs[12] == "X") && (pegs[13] == "X") && (pegs[14] == "X") && (pegs[15] == "X"))
     {
         return true;
     }
+
+    // O row win
+    if      ( (pegs[0] == "O") && (pegs[1] == "O") && (pegs[2] == "O") && (pegs[3] == "O"))
+    {
+        return true;
+    }
+    else if ( (pegs[4] == "O") && (pegs[5] == "O") && (pegs[6] == "O") && (pegs[7] == "O"))
+    {
+        return true;
+    }
+    else if ( (pegs[8] == "O") && (pegs[9] == "O") && (pegs[10] == "O") && (pegs[11] == "O"))
+    {
+        return true;
+    }
+    else if ( (pegs[12] == "O") && (pegs[13] == "O") && (pegs[14] == "O") && (pegs[15] == "O"))
+    {
+        return true;
+    }
+
     else
     {
         return false;
@@ -81,14 +121,26 @@ false
 */
 bool TicTacToe4::check_diagonal_win()
 {
-    if      ( (pegs[0] == "X" || pegs[0] == "O") && (pegs[5] == "X" || pegs[5] == "O") && (pegs[10] == "X" || pegs[10] == "O") && (pegs[15] == "X" || pegs[15] == "O"))
+    // X diagonal win
+    if      ( (pegs[0] == "X") && (pegs[10] == "X") && (pegs[5] == "X") && (pegs[15] == "X"))
     {
         return true;
     }
-    else if ( (pegs[3] == "X" || pegs[3] == "O") && (pegs[6] == "X" || pegs[6] == "O") && (pegs[9] == "X" || pegs[9] == "O") && (pegs[12] == "X" || pegs[12] == "O"))
+    else if ( (pegs[3] == "X") && (pegs[6] == "X") && (pegs[9] == "X") && (pegs[12] == "X"))
     {
         return true;
     }
+
+    // O diagonal win
+    else if ( (pegs[3] == "O") && (pegs[6] == "O") && (pegs[9] == "O") && (pegs[12] == "O"))
+    {
+        return true;
+    }
+    else if ( (pegs[3] == "O") && (pegs[6] == "O") && (pegs[9] == "O") && (pegs[12] == "O"))
+    {
+        return true;
+    }
+    
     else
     {
         return false;
