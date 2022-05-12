54804eed24dc09bee477b7be352c30c7347f77f7
lucian-swaim-acc <lucian.swaim@g.austincc.edu>
2022-05-12 05:56:12 -0500

    Question 4 Test Cases v03

--- src/examples/05_module/die.cpp
+++ src/examples/05_module/die.cpp
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 void Die::roll() {
-    const int MAX = 6;
+    const int MAX = sides;
     const int MIN = 1;
     int range = MAX - MIN + 1;
     roll_value = rand() % range + MIN;
