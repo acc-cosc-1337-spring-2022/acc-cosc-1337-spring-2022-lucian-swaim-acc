7845816cc6f5b3130110b263fd44500a2b33a517
lucian-swaim-acc <lucian.swaim@g.austincc.edu>
2022-05-12 05:54:01 -0500

    Question 4 Test Cases v02

--- src/examples/05_module/come_out_phase.cpp
+++ src/examples/05_module/come_out_phase.cpp
@@ -1,6 +1,6 @@
 #include "come_out_phase.h"
 
-Phase::RollOutcome get_outcome(Roll* roll)
+Phase::RollOutcome ComeOutPhase::get_outcome(Roll* roll)
 {
     if ((roll->roll_value() == 7) || (roll->roll_value() == 11))
     {
--- src/examples/05_module/main.cpp
+++ src/examples/05_module/main.cpp
@@ -1,6 +1,48 @@
+#include "die.h"
+#include "roll.h"
+#include "shooter.h"
+#include "phase.h"
+#include "point_phase.h"
+#include "come_out_phase.h"
+#include "time.h"
+
+#include <iostream>
+using std::cout;
+
 
 int main()
 {
+    srand(time(0));
 
-    return 0;
+    Die die1;
+    Die die2;
+    Shooter shooter;
+    Roll* roll;
+    roll = shooter.throw_die(die1, die2);
+
+    ComeOutPhase come_out_phase;
+
+    while ( (come_out_phase.get_outcome(roll) == Phase::RollOutcome::natural) || (come_out_phase.get_outcome(roll) == Phase::RollOutcome::craps) )
+    {
+        cout << "Rolled " << roll->roll_value() << " roll again\n";
+        roll = shooter.throw_die(die1, die2);
+    }
+
+    cout << "Rolled " << roll->roll_value() << " start of point phase\n";
+    cout << "Roll until " << roll->roll_value() << " or 7 is rolled\n";
+
+    int point = roll->roll_value();
+    roll = shooter.throw_die(die1, die2);
+
+    PointPhase point_phase(point);
+
+    while ( (point_phase.get_outcome(roll) == Phase::RollOutcome::point) || (point_phase.get_outcome(roll) == Phase::RollOutcome::nopoint) )
+    {
+        cout << "Rolled " << roll->roll_value() << " roll again\n";
+        roll = shooter.throw_die(die1, die2);
+    }
+
+    cout << "Rolled " << roll->roll_value() << " end of point phase\n";
+
+    cout << shooter;
 }
